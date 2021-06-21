a80d3f477b8b9cac87bbd9cd24a8fc7ebc21f424
Alexandr Kislyakov <kislyakov.sk@yandex.ru>
2021-06-21 19:35:49 +0300

    Fix name style file textarea component

--- components/Textarea/Textarea.tsx
+++ components/Textarea/Textarea.tsx
@@ -1,7 +1,7 @@
 import cls from 'clsx';
 
 import { TextareaProps } from './Textarea.props';
-import styles from './Input.module.css';
+import styles from './Textarea.module.css';
 
 const Textarea: React.FC<TextareaProps> = ({ className, ...props }) => {
     return <textarea className={cls(styles.textarea, className)} {...props} />;
