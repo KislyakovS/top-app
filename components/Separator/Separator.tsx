0c6f91a0519a8c451a53e4a3aa9fad6f0e88da67
Alexandr Kislyakov <kislyakov.sk@yandex.ru>
2021-06-23 19:20:50 +0300

    Fix

--- components/Separator/Separator.tsx
+++ components/Separator/Separator.tsx
@@ -3,6 +3,6 @@ import cls from 'clsx';
 import { SeparatorProps } from './Separator.props';
 import styles from './Separator.module.css';
 
-const Separator: React.FC<SeparatorProps> = ({ className }) => <div className={cls(styles.separator, className)}></div>;
+const Separator: React.FC<SeparatorProps> = ({ className, ...props }) => <div className={cls(styles.separator, className)} {...props}></div>;
 
 export default Separator;
